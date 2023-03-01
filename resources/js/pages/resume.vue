e90387f6e8e4586f2e164e4b9cbc52dbb747d60b
Kevin Wheeler <spreadlove5683@gmail.com>
2023-03-01 17:23:13 +0000

    PDF embed working even with multiple router navigations. If there is a way to make it not have to reload each time though, that would be ideal.

--- resources/js/pages/resume.vue
+++ resources/js/pages/resume.vue
@@ -3,7 +3,7 @@
         <div class="flex flex-col gap-6">
             <h1 class="text-6xl font-bold text-center">Rés<span class="bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-transparent">umé</span></h1>
             <div class="text-l text-gray-400 self-center text-center">Last Updated March 2023</div>
-            <div class="w-full mb-12" id="adobe-dc-view"></div>
+            <div class="w-full mb-12" id="pdfview"></div>
             <!-- <embed class="w-full min-h-screen" src="https://drive.google.com/viewerng/
 viewer?embedded=true&url=https://www.kevinwheeler.net/storage/kevin-wheeler-resume.pdf"> -->
             <a href="/storage/kevin-wheeler-resume.pdf" class="w-full self-center px-6 py-2.5 mb-24 text-center text-white font-medium text-xs
@@ -18,15 +18,45 @@ viewer?embedded=true&url=https://www.kevinwheeler.net/storage/kevin-wheeler-resu
 </template>
 
 <script>
-import { onMounted } from '@vue/runtime-core'
+import { onMounted, ref, watch } from '@vue/runtime-core'
 
 export default {
     
     components: {
     },
     setup() {
+        const adobeApiPDFReady = ref(false);
         onMounted(() => {
-        })
+            document.addEventListener("adobe_dc_view_sdk.ready", () => {
+            	adobeApiPDFReady.value = true;
+            	console.log("Adobe created with adobe_dc_view_sdk.ready");
+            });
+            
+            // Dynamically load Adobe SDK Viewer for this page
+            const plugin = document.createElement("script");
+            plugin.setAttribute(
+            	"src",
+            	"https://documentcloud.adobe.com/view-sdk/viewer.js"
+            );
+            plugin.async = true;
+            document.head.appendChild(plugin);
+        });
+
+        watch([adobeApiPDFReady], (val)=> {
+            if (val) {
+			    // val == true ; Adobe is loaded on page
+			    let adobeDCView = new window.AdobeDC.View({
+			    	clientId: "34e74ab26db84d84936a007c215330e8",
+			    	divId: "pdfview",
+			    	});
+                    adobeDCView.previewFile({
+            			content:{location: {url: "/storage/kevin-wheeler-resume.pdf"}},
+            			metaData:{fileName: "kevin-wheeler-resume.pdf"}
+            		}, {embedMode: "IN_LINE"});
+                    // adobeDCView.previewFile
+			    console.log("Adobe is mounted with Client ID");
+			}
+        });
 
         return {
         }
--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -22,18 +22,6 @@
               });
            });
         </script> --}}
-        
-<script defer src="https://documentservices.adobe.com/view-sdk/viewer.js"></script>
-<script defer type="text/javascript">
-	document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
-		var adobeDCView = new AdobeDC.View({clientId: "34e74ab26db84d84936a007c215330e8", divId: "adobe-dc-view"});
-		adobeDCView.previewFile({
-			// content:{location: {url: "https://www.kevinwheeler.net/storage/kevin-wheeler-resume.pdf"}},
-			content:{location: {url: "/storage/kevin-wheeler-resume.pdf"}},
-			metaData:{fileName: "Bodea Brochure.pdf"}
-		}, {embedMode: "IN_LINE"});
-	});
-</script>
     </head>
     <body >
         <div id="app"></div>
