ca58547b8e5cb3efeb89893e00d1a9ea78be3ecc
Kevin Wheeler <spreadlove5683@gmail.com>
2023-06-11 22:57:48 +0000

    Factored out head items and added FB Pixel Code.

---
+++ resources/views/components/shared-head-items.blade.php
@@ -0,0 +1,32 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>{{$title}}</title>
+<meta name="description" content="{{$description}}">
+
+<link rel="icon" type="image/svg+xml" href="/storage/favicon.svg">
+<link rel="icon" type="image/png" href="/storage/favicon.png">
+
+<!-- Meta Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '213065001127118');
+  fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=213065001127118&ev=PageView&noscript=1"
+  /></noscript>
+  <!-- End Meta Pixel Code -->
+
+@vite(['resources/js/app.js', 'resources/sass/app.scss'])
+
+@if($devEnvironment)
+    <script src="https://cdn.tailwindcss.com"></script>
+@endif
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>{{$title}}</title>
-        <meta name="description" content="{{$description}}">
-        <link rel="icon" type="image/svg+xml" href="/storage/favicon.svg">
-        <link rel="icon" type="image/png" href="/storage/favicon.png">
-        @vite(['resources/js/app.js', 'resources/sass/app.scss'])
-
-        @if($devEnvironment)
-            <script src="https://cdn.tailwindcss.com"></script>
-        @endif
+        <x-shared-head-items title="{{$title}}" description="{{$description}}" devEnvironment="{{$devEnvironment}}">
+        </x-shared-head-items>
     </head>
     <body >
         <div id="app"></div>
