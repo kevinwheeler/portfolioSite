344e497f07be0ba4d08e0e4163e09af3b82f736c
Kevin Wheeler <spreadlove5683@gmail.com>
2023-08-09 18:02:48 +0000

    Going into safe area on iphone now.

--- resources/views/components/shared-head-items.blade.php
+++ resources/views/components/shared-head-items.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 
 <title>{{$title}}</title>
 <meta name="description" content="{{$description}}">
